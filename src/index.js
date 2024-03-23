d7d9930877df9ca24ddf61bda07778f4f3fe86cf
leeanson19 <leeanson19@gmail.com>
2024-03-23 10:47:52 -0700

    Add CORS support

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import express from 'express';
 import articleRouter from './router/articleRouter.js';
+import cors from 'cors';
 
 const app = express();
-
-const ARTICLE_API_ENDPOINT = 'article';
-
-app.use(`/${ARTICLE_API_ENDPOINT}`, articleRouter);
-
-
 const PORT = 8080;
 
+const ARTICLE_API_ENDPOINT = 'article';
+const CORS_OPTIONS = {
+  origin: "*",
+  methods: "GET,POST,PUT,DELETE"
+};
+
+app.use(cors());
+
+
+app.use(`/${ARTICLE_API_ENDPOINT}`, articleRouter);
 
 
 app.listen(PORT, () => {
