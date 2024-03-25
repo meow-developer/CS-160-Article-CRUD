10c5d05fcd17e44ef6d239cc01fe2ba3c615f766
leeanson19 <leeanson19@gmail.com>
2024-03-25 16:33:49 -0700

    Add documentation for controller

--- src/controller/articleController.js
+++ src/controller/articleController.js
@@ -1,23 +1,37 @@
+import { Request, Response } from 'express';
+
 /**
  * Creating a new article
  * HTTP Method: POST
  * Path: /article
+ * 
+ * @param {Request} req - Request object
+ * @param {Response} res - Response object
+ * @returns {void}
  */
 const createArticle = (req, res) => {
 
 };
 
-
 /**
  * Fetching a list of articles
  * HTTP Method: GET
  * Path: /article
+ * 
+ * @param {Request} req - Request object
+ * @param {Response} res - Response object
+ * @returns {void}
  */
 const listArticle = (req, res) => {
 
     const query = req.query;
 
+    // Default limit for the number of articles to fetch
     const DEFAULT_ARTICLE_COUNT_LIMIT = 10;
+    /** 
+    * Fetching the limit from the query parameters
+    * If the limit is not provided, the default limit will be used 
+    */
     const articleCountLimit = query.limit || DEFAULT_ARTICLE_COUNT_LIMIT;
 
 
@@ -28,6 +42,10 @@ const listArticle = (req, res) => {
  * Fetching a single article
  * HTTP Method: GET
  * Path: /article/:articleId
+ * 
+ * @param {Request} req - Request object
+ * @param {Response} res - Response object
+ * @returns {void}
  */
 const getArticle = (req, res) => {
     const articleId = req.params.articleId;
@@ -39,6 +57,10 @@ const getArticle = (req, res) => {
  * Updating an article
  * HTTP Method: PUT
  * Path: /article/:articleId
+ * 
+ * @param {Request} req - Request object
+ * @param {Response} res - Response object
+ * @returns {void}
  */
 const updateArticle = (req, res) => {
     const articleId = req.params.articleId;
@@ -49,10 +71,16 @@ const updateArticle = (req, res) => {
  * Deleting an article
  * HTTP Method: DELETE
  * Path: /article/:articleId
+ * 
+ * @param {Request} req - Request object
+ * @param {Response} res - Response object
+ * @returns {void}
  */
 
 const deleteArticle = (req, res) => {
+    // Extracting the articleId from the request parameters
     const articleId = req.params.articleId;
+
 
 };
 
