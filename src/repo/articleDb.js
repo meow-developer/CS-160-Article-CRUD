3fef8ed9eded643968cf7d91b90dfc48a3cbd4f8
leeanson19 <leeanson19@gmail.com>
2024-04-07 15:51:32 -0700

    Fix private access modifiers

--- src/repo/articleDb.js
+++ src/repo/articleDb.js
@@ -2,8 +2,8 @@ import MysqlDatabase from './dbConn.js'
 
 export default class ArticleDb {
     static instance;
-    mysqlDb = MysqlDatabase.getInstance();
-    dbPool = this.mysqlDb.getPool();
+    #mysqlDb = MysqlDatabase.getInstance();
+    #dbPool = this.#mysqlDb.getPool();
 
     constructor() {
 
@@ -19,7 +19,7 @@ export default class ArticleDb {
     async insertArticle(article) {
         let conn;
         try {
-            conn = await this.dbPool.getConnection();
+            conn = await this.#dbPool.getConnection();
             const query = `INSERT INTO Articles (Title, StorageArticleUUID, Active, ArticleID) VALUES (?, ?, ?)`;
             const [result] = await conn.execute(query, [article.title, article.storageArticleUUID, article.active]);
             return result;
@@ -34,7 +34,7 @@ export default class ArticleDb {
     async updateArticle(articleId, article) {
         let conn;
         try {
-            conn = await this.dbPool.getConnection();
+            conn = await this.#dbPool.getConnection();
             const query = `UPDATE Articles SET Title = ?, StorageArticleUUID = ?, Active = ? WHERE ArticleId = ?`;
             const [result] = await conn.execute(query, [article.title, article.storageArticleUUID, article.active, articleId]);
             return result;
@@ -47,7 +47,7 @@ export default class ArticleDb {
     async getArticle(articleId) {
         let conn;
         try {
-            conn = await this.dbPool.getConnection();
+            conn = await this.#dbPool.getConnection();
             const query = `SELECT * FROM Articles WHERE ArticleId = ?`;
             const [result] = await conn.execute(query, [articleId]);
             return result;
@@ -60,7 +60,7 @@ export default class ArticleDb {
     async countArticleByStorageUUID(storageArticleUUID) {
         let conn;
         try {
-            conn = await this.dbPool.getConnection();
+            conn = await this.#dbPool.getConnection();
             const query = `SELECT COUNT(*) as count FROM Articles WHERE StorageArticleUUID = ?`;
             const [result] = await conn.execute(query, [storageArticleUUID]);
             return result[0].count;
@@ -73,7 +73,7 @@ export default class ArticleDb {
     async deleteArticle(articleId) {
         let conn;
         try {
-            conn = await this.dbPool.getConnection();
+            conn = await this.#dbPool.getConnection();
             const query = `DELETE FROM Articles WHERE ArticleId = ?`;
             const [result] = await conn.execute(query, [articleId]);
             return result;
@@ -87,7 +87,7 @@ export default class ArticleDb {
     async deleteArticleByStorageUUID(storageArticleUUID) {
         let conn;
         try {
-            conn = await this.dbPool.getConnection();
+            conn = await this.#dbPool.getConnection();
             const query = `DELETE FROM Articles WHERE StorageArticleUUID = ?`;
             const [result] = await conn.execute(query, [storageArticleUUID]);
             return result;
