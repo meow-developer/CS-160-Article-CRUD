be6d83d6e3641051704de5ca4639fb68e85d61b1
leeanson19 <leeanson19@gmail.com>
2024-04-07 11:47:05 -0700

    Add tests

--- test/repo/dbConn.test.ts
+++ test/repo/dbConn.test.ts
@@ -2,7 +2,6 @@ import MysqlDatabase from '../../src/repo/dbConn';
 
 describe("MySQL Database connection", ()=>{
     const db = MysqlDatabase.getInstance();
-    db.createPool();
 
     test("Ping Test", async ()=>{
         const ping = await db.testPoolConnection();
