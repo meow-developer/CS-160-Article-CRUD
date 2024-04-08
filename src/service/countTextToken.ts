beb34e1446f8f1ea82a9208fc5a01edc7f9469b6
leeanson19 <leeanson19@gmail.com>
2024-04-07 21:01:40 -0700

    Remove token limit in token counter

--- src/service/countTextToken.ts
+++ src/service/countTextToken.ts
@@ -2,7 +2,6 @@ import openAiTokenCounter, { ModelType } from 'openai-gpt-token-counter';
 
 export default class TextTokenCounterService {
     CHATGPT_MODEL: ModelType = "gpt-3.5-turbo";
-    TOKEN_LIMIT = 3500;
     articleText: string;
 
     constructor(articleText: string) {
