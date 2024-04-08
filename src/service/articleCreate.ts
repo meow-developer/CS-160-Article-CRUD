ae08b4d95ce3103c0e5aef1643146067efa1a64f
leeanson19 <leeanson19@gmail.com>
2024-04-08 15:40:48 -0700

    Add Ariicle Store UUID Collision Handler

--- src/service/articleCreate.ts
+++ src/service/articleCreate.ts
@@ -17,13 +17,28 @@ export default class ArticleCreateService {
         return crypto.randomUUID();
     }
 
-    private async checkUUIDDbCollision(uuid: UUID) {
+    private async checkUUIDDbCollision(uuid: UUID): Promise<boolean> {
         const articleCount = await this.articleDb.countArticleByStorageUUID(uuid);
-        const isUUIDCollision = articleCount > 0;
+        const isUUIDCollided = articleCount > 0;
         
-        if(isUUIDCollision){
-            throw new ArticleCreateError("UUID collision");
-        }
+        return isUUIDCollided;
+    }
+
+    private async safeUUIDGeneration(): Promise<UUID>{
+        const MAX_TRIES = 2; //It's impossible to have a collision with 2 tries
+        let tries = 0;
+       
+        do {
+            const uuid = this.generateUUIDForArticleStore();
+            const isUUIDCollision = await this.checkUUIDDbCollision(uuid);
+
+            if (!isUUIDCollision) {
+                return uuid;
+            }
+            tries++;
+        } while (tries < MAX_TRIES);
+
+        throw new ArticleCreateError("Failed to generate UUID");
     }
 
     private async writeArticleToDb(
@@ -54,19 +69,16 @@ export default class ArticleCreateService {
             articleStorageUUID + ".pdf",
             articleFilePath
         )
-        console.log("Article saved to storage")
     }
 
     public async save(file: Express.Multer.File){
-        const articleStorageUUID = this.generateUUIDForArticleStore();
+        const articleStorageUUID = await this.safeUUIDGeneration();
         try {
-            const isUUIDCollision = await this.checkUUIDDbCollision(articleStorageUUID);
 
             await this.writeArticleToDb(file.originalname, articleStorageUUID);
             await this.saveArticleToStorage(articleStorageUUID, file.path);
-        } catch (err) {
-            console.log(err)
 
+        } catch (err) {
             //Ensure atomicity
             //Undo all the changes made to the db and storage
             await this.deleteArticleFromDb(articleStorageUUID);
